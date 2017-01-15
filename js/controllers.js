75e5b34976192f8bfe6fc08a979209bc50904891
Ever Garcia <uriel621@live.com>
2017-01-14 22:01:53 -0800

    updated json file

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,13 @@
 var fighterControllers = angular.module("fighterControllers", []);
 
 fighterControllers.controller("ListController", ["$scope", "$http", function ($scope, $http){
-  $http.get("js/data.json").success(function(data){
+  $http.get("js/directory.json").success(function(data){
     $scope.SSB4_Fighters = data;
   })
 }]);
 
 fighterControllers.controller("InformationController", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams){
-  $http.get("js/data.json").success(function(data){
+  $http.get("js/directory.json").success(function(data){
     $scope.SSB4_Fighters = data;
     $scope.whichItem = $routeParams.itemId;
   })
